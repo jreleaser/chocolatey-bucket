d4a38a10bafe8624b875ab3cf34af8034ff315c7
jreleaserbot <jreleaser@kordamp.org>
2021-11-27 18:02:14 +0000

    jreleaser v0.9.0

--- jreleaser/tools/chocolateyinstall.ps1
+++ jreleaser/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $app_bat = Join-Path $app_home 'bin/jreleaser.bat'
 Install-ChocolateyZipPackage `
     -PackageName 'jreleaser' `
     -Url 'https://github.com/jreleaser/jreleaser/releases/download/v0.9.0/jreleaser-0.9.0.zip' `
-    -Checksum '91d4f7df46122d83dd77029f290f69191d6305a9142c059be458f8cd1f4f7838' `
+    -Checksum '2a0e82f59400bfa0e87ed42ca10a824ce830d856df28a944e1ae1467cdad18fa' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
