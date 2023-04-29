55983734df939c0efc5e687f7c646ddb51d23380
jreleaserbot <jreleaser@kordamp.org>
2023-04-29 10:21:47 +0000

    jreleaser v1.6.0

--- jreleaser/tools/chocolateyinstall.ps1
+++ jreleaser/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-# Generated with JReleaser 1.6.0-SNAPSHOT at 2023-03-12T10:15:10.135700883Z
+# Generated with JReleaser 1.6.0-SNAPSHOT at 2023-04-29T10:13:03.544759325Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'jreleaser-1.5.1'
+$app_home = Join-Path $package 'jreleaser-1.6.0'
 $app_exe = Join-Path $app_home 'bin/jreleaser.bat'
 
 Install-ChocolateyZipPackage `
     -PackageName 'jreleaser' `
-    -Url 'https://github.com/jreleaser/jreleaser/releases/download/v1.5.1/jreleaser-1.5.1.zip' `
-    -Checksum 'd13384f83a16526ea0c4ef4d5e4f9d637b7100ddd470a637ac361245151c8340' `
+    -Url 'https://github.com/jreleaser/jreleaser/releases/download/v1.6.0/jreleaser-1.6.0.zip' `
+    -Checksum '58a0593ecce9e2acad2851374d24dc8180bfbe6dfe48de35a1d1ba75747df1b1' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
--- jreleaser/tools/chocolateyuninstall.ps1
+++ jreleaser/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
-# Generated with JReleaser 1.6.0-SNAPSHOT at 2023-03-12T10:15:10.135700883Z
+# Generated with JReleaser 1.6.0-SNAPSHOT at 2023-04-29T10:13:03.544759325Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'jreleaser-1.5.1'
+$app_home = Join-Path $package 'jreleaser-1.6.0'
 $app_exe = Join-Path $app_home 'bin/jreleaser.bat'
 
 Uninstall-BinFile -Name 'jreleaser' -Path $app_exe
